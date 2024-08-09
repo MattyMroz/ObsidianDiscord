b6e78d76465961a421ab094233403afcb52f6966
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2024-08-09 16:47:15 +0200

    ObsidianDiscord.js 1.6

--- ObsidianDiscord.js
+++ ObsidianDiscord.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Obsidian Discord Theme
 // @namespace    http://tampermonkey.net/
-// @version      1.3
-// @description  Dodaje motyw Obsidian do Discorda po załadowaniu strony, 10 razy w odstępach 1-sekundowych, a następnie przy zmianie paska adresu
+// @version      1.5
+// @description  Dodaje motyw Obsidian do Discorda, intensywnie odświeża na początku, potem monitoruje zmiany URL
 // @author       Matty_Mroz
 // @match        https://discord.com/*
 // @grant        GM_addStyle
@@ -18,13 +18,20 @@
         'https://mattymroz.github.io/ObsidianDiscord/ObsidianDiscordThemeOnline.css',
     ];
 
+    let styleElement = null;
+    let lastUrl = location.href;
+    let initialLoadCount = 0;
+
     function loadCSS(url) {
         return new Promise((resolve, reject) => {
             GM_xmlhttpRequest({
                 method: 'GET',
                 url: url,
                 onload: function (response) {
-                    GM_addStyle(response.responseText);
+                    if (styleElement) {
+                        styleElement.remove();
+                    }
+                    styleElement = GM_addStyle(response.responseText);
                     resolve();
                 },
                 onerror: reject
@@ -39,20 +46,44 @@
             });
     }
 
-    function changeAddressBar() {
-        const newUrl = window.location.href + '#obsidian-theme';
-        window.history.pushState({}, '', newUrl);
-    }
-
-    function repeatedLoad(times) {
-        if (times > 0) {
+    function checkUrlChange() {
+        if (lastUrl !== location.href) {
+            lastUrl = location.href;
             loadAllCSS();
-            setTimeout(() => repeatedLoad(times - 1), 1000);
-        } else {
-            changeAddressBar();
         }
     }
 
-    window.addEventListener('load', () => repeatedLoad(10));
+    function initialLoad() {
+        loadAllCSS();
+        initialLoadCount++;
+        if (initialLoadCount < 10) {
+            setTimeout(initialLoad, 500);
+        } else {
+            setInterval(checkUrlChange, 100);
+        }
+    }
+
+    function init() {
+        loadAllCSS();
+        initialLoad();
+
+        window.addEventListener('popstate', loadAllCSS);
+        const originalPushState = history.pushState;
+        const originalReplaceState = history.replaceState;
+
+        history.pushState = function () {
+            originalPushState.apply(this, arguments);
+            loadAllCSS();
+        };
+
+        history.replaceState = function () {
+            originalReplaceState.apply(this, arguments);
+            loadAllCSS();
+        };
+
+        window.addEventListener('hashchange', loadAllCSS);
+    }
+
+    window.addEventListener('load', init);
 
 })();
