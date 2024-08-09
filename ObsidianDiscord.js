a231a962c0a312963e1588b766c175250e6fe818
Mateusz Mróz <71221358+MattyMroz@users.noreply.github.com>
2024-08-09 16:09:17 +0200

    ObsidianDiscord.js 1.3

--- ObsidianDiscord.js
+++ ObsidianDiscord.js
@@ -2,7 +2,7 @@
 // @name         Obsidian Discord Theme
 // @namespace    http://tampermonkey.net/
 // @version      1.3
-// @description  Dodaje motyw Obsidian do Discorda po załadowaniu strony, 10 razy w odstępach 1-sekundowych
+// @description  Dodaje motyw Obsidian do Discorda po załadowaniu strony, 10 razy w odstępach 1-sekundowych, a następnie zmienia pasek adresu
 // @author       Matty_Mroz
 // @match        https://discord.com/*
 // @grant        GM_addStyle
@@ -39,10 +39,17 @@
             });
     }
 
+    function changeAddressBar() {
+        const newUrl = window.location.href + '#obsidian-theme';
+        window.history.pushState({}, '', newUrl);
+    }
+
     function repeatedLoad(times) {
         if (times > 0) {
             loadAllCSS();
-            setTimeout(() => repeatedLoad(times - 1), 500);
+            setTimeout(() => repeatedLoad(times - 1), 1000);
+        } else {
+            changeAddressBar();
         }
     }
 
